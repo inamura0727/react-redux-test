f5bb3b7fc5ae1e46ccdcb2bc5b3e648bc6ae64ab
Inamura Tamaki <tinamura0159@gmail.com>
2023-03-27 16:31:41 +0900

    POST機能追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import { act } from 'react-dom/test-utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchJSONPost, selectStatus } from '../features/todo/todoSlice';
+import { AppDispatch } from '../store';
+import Spinner from './Spinner';
 
 const Form = () => {
   const [text, setText] = useState('');
+  const [isWritten, setIsWritten] = useState(false);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const status = useSelector(selectStatus);
+  console.log(status);
 
   const handleCahnge = (e: {
     target: { value: React.SetStateAction<string> };
   }) => {
     // actで囲んでってエラーが出たので...
-    act(()=>setText(e.target.value));
+    act(() => setText(e.target.value));
+  };
+
+  const handleClick = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (text === '') {
+      setIsWritten(true);
+      return;
+    }
+    dispatch(fetchJSONPost(text));
+    setIsWritten(false);
+    setText('');
   };
 
   return (
@@ -22,7 +42,13 @@ const Form = () => {
           value={text}
         />
         <div className="btn_wrapper">
-          <button className="btn">Add</button>
+          {isWritten && (
+            <p className="alermText">Please fill something in this form!</p>
+          )}
+          <button className="btn" onClick={handleClick}>
+            Add
+          </button>
+          {status === 'Pending' && <Spinner />}
         </div>
       </form>
     </div>
